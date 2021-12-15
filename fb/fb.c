e267d80d1752b542b9388525971c31cf4c899192
MBStarup <mavi.nexu@gmail.com>
2021-12-15 23:11:45 +0100

    Somethings wrong, the result doesnt' add up correctly

--- fb/fb.c
+++ fb/fb.c
@@ -4,18 +4,38 @@
 #include <string.h>
 #include <stdbool.h>
 
+/*SECTION ht.h*/
+
+/*
+Notes (for me) (more like pet peeves)
+
+Difference between first order entries and secondorder entries, both in handling and allocation, is kinda cringe (maybe just bite the bullet and have each entry alloced on it's own?)
+Are the pointers even worth it? 8 BYTES!?! (We know the location anyways from the sizes, but I guess the shorthand is nice)
+Figure out if the pointers can be made constant as we should NEVER change the pointer if we remove the different treatment of first order entries (right now we use the key pointer as a flag to see if an entry is empty)
+*/
+
+/*
+Adds bytes amount of bytes to pointer
+*/
 void *addPtr(const void *pointer, int bytes)
 {
     return (((char *)pointer) + bytes);
 }
 
+/*
+The struct representing a hashtable
+This struct does not actually contain the data of the hashtable, only the metadate about the hashtable and a reference to the actual data
+
+void* data: the reference to the data, this will be dynamically allocated in htInit()
+int tableSize: The amount of first order entries in the table, this
+*/
 typedef struct hashTable
 {
-    // hashTableEntry *table;
-    void *data;
-    int tableSize;
-    int keySize;
-    int valueSize;
+    const void *data;
+    const int tableSize;
+    const int keySize;
+    const int valueSize;
+    int count;
 } hashTable;
 
 typedef struct hashTableEntry
@@ -25,87 +45,33 @@ typedef struct hashTableEntry
     struct hashTableEntry *next;
 } hashTableEntry;
 
-unsigned int _htHashFunc(void *key, int size) /*quite probably bad and very bad hashing function*/ // TODO: implement something better :), ow don't idk
+unsigned int _htHashFunc(void *key, int size)
 {
+    /*quite probably bad hashing function*/
+    /*TODO: Implement a real one*/
     unsigned int res = 0;
     for (size_t i = 0; i < size; i++)
     {
         res += (unsigned int)*(char *)(addPtr(key, i));
     }
     return res;
-
-    // algorithm fnv-1a is
-    // hash := FNV_offset_basis
-
-    // for each byte_of_data to be hashed do
-    //     hash := hash XOR byte_of_data
-    //     hash := hash × FNV_prime
-
-    // return hash
 }
 
-unsigned int _htGetIndex(hashTable table, void *key)
+unsigned int _htGetIndex(const hashTable *table, void *key)
 {
-    return _htHashFunc(key, table.keySize) % table.tableSize;
+    return _htHashFunc(key, table->keySize) % table->tableSize;
 }
 
-int _htGetOffset(hashTable table, int index)
+int _htGetOffset(const hashTable *table, int index)
 {
-    return index * ((sizeof(hashTableEntry) + table.keySize + table.valueSize));
+    return index * ((sizeof(hashTableEntry) + table->keySize + table->valueSize));
 }
-
-// // hashTableEntry *_htGetEntry(hashTable table, void *key)
-// // {
-// //     /* Recursive way
-// //     // hashTableEntry *_htRecSearchEntry(hashTableEntry * entry, void *key)
-// //     // {
-// //     //     if (memcmp(entry->key, key) == 0)
-// //     //     {
-// //     //         return entry;
-// //     //     }
-// //     //     else if (entry->next != NULL)
-// //     //     {
-// //     //         return _htRecSearchEntry(entry->next, key);
-// //     //     }
-// //     //     else
-// //     //     {
-// //     //         return NULL
-// //     //     }
-// //     // }
-
-// //     // hashTableEntry *entry = table.data + _htGetOffset(_htHashFunc(key, table.keySize));
-// //     // return _htRecSearchEntry(entry, key);
-// //     */
-
-// //     hashTableEntry *entry = addPtr(table.data, _htGetOffset(_htHashFunc(key, table.keySize)));
-// //     while (memcmp(entry->key, key) != 0)
-// //     {
-// //         if (entry->next != NULL)
-// //         {
-// //             entry = entry->next;
-// //         }
-// //         else
-// //         {
-// //             return NULL
-// //         }
-// //     }
-// //     return entry;
-// // }
 
 hashTable htInitTable(int tableSize, int keySize, int valueSize)
 {
-    hashTable table = {.tableSize = tableSize, .keySize = keySize, .valueSize = valueSize};
-
-    /*
-    // table.table = malloc(tableSize * (sizeof(hashTableEntry)));
-    // assert(table.table != NULL);
-
-    // void *data = malloc(tableSize * (sizeof(hashTableEntry) + keySize + valueSize)); //Allocate the initial chunk of memory
-    // assert(data != NULL);
-    */
-
-    table.data = malloc(tableSize * (sizeof(hashTableEntry) + keySize + valueSize)); /*Allocate the initial chunk of memory*/
-    assert(table.data != NULL);
+    void *ptr = malloc(tableSize * (sizeof(hashTableEntry) + keySize + valueSize)); /*Allocate the initial chunk of memory*/
+    assert(ptr != NULL);
+    hashTable table = {.data = ptr, .tableSize = tableSize, .keySize = keySize, .valueSize = valueSize, .count = 0};
 
     for (size_t i = 0; i < table.tableSize; i++)
     {
@@ -122,9 +88,9 @@ void htDeleteTable(hashTable table)
     assert(0 && "Not implemented yet"); // TODO: implement
 }
 
-void *htGet(hashTable table, void *key)
+void *htGet(const hashTable *table, void *key)
 {
-    hashTableEntry *entry = addPtr(table.data, _htGetOffset(table, _htGetIndex(table, key)));
+    hashTableEntry *entry = addPtr(table->data, _htGetOffset(table, _htGetIndex(table, key)));
     if (entry->key == NULL)
         return NULL; /*Key not in table*/
     else
@@ -132,7 +98,7 @@ void *htGet(hashTable table, void *key)
         hashTableEntry *next = NULL;
         do
         {
-            if (memcmp(entry->key, key, table.keySize) == 0) /*Found it*/
+            if (memcmp(entry->key, key, table->keySize) == 0) /*Found it*/
             {
                 return entry->value;
             }
@@ -143,25 +109,21 @@ void *htGet(hashTable table, void *key)
     }
 }
 
-bool htContains(hashTable table, void *key)
-{
-    return htGet(table, key) == NULL ? false : true;
-}
-
 /*
 Inserts an value in the hashtable
 If an value already exists at teh given key, it will be replaced
 
 returns 1 for success and 0 for faliure
 */
-int htInsert(hashTable table, void *key, void *value)
+int htInsert(hashTable *table, void *key, void *value)
 {
-    hashTableEntry *entry = addPtr(table.data, _htGetOffset(table, _htGetIndex(table, key)));
+    hashTableEntry *entry = addPtr(table->data, _htGetOffset(table, _htGetIndex(table, key)));
     if (entry->key == NULL) /*Empty slot*/
     {
-        entry->key = memcpy(addPtr(entry, sizeof(hashTableEntry)), key, table.keySize);                       /*Copy the key into the area after the entry*/
-        entry->value = memcpy(addPtr(entry, sizeof(hashTableEntry) + table.keySize), value, table.valueSize); /*Copy the value into the area after the key*/
-        entry->next = NULL;                                                                                   /*Make sure the next is set to NULL, to indicate that this is the last entry at the position*/
+        entry->key = memcpy(addPtr(entry, sizeof(hashTableEntry)), key, table->keySize);                        /*Copy the key into the area after the entry*/
+        entry->value = memcpy(addPtr(entry, sizeof(hashTableEntry) + table->keySize), value, table->valueSize); /*Copy the value into the area after the key*/
+        entry->next = NULL;                                                                                     /*Make sure the next is set to NULL, to indicate that this is the last entry at the position*/
+        table->count += 1;
         return 1;
     }
     else
@@ -169,33 +131,34 @@ int htInsert(hashTable table, void *key, void *value)
         hashTableEntry *next = NULL;
         do
         {
-            if (memcmp(entry->key, key, table.keySize) == 0) /*Already existing entry*/
+            if (memcmp(entry->key, key, table->keySize) == 0) /*Already existing entry*/
             {
-                memcpy(entry->value, value, table.valueSize); /*overide the value with the new value*/
+                memcpy(entry->value, value, table->valueSize); /*overide the value with the new value*/
                 return 1;
             }
             next = entry->next;
         } while (next != NULL && (entry = next) /*Funky code: only assign entry to next if next is not NULL*/);
 
-        next = malloc(sizeof(hashTableEntry) + table.keySize + table.valueSize);
+        next = malloc(sizeof(hashTableEntry) + table->keySize + table->valueSize);
         if (next == NULL)
-            return 0;                                                                                       /*Failed to allocate memory for the new entry*/
-        next->key = memcpy(addPtr(next, sizeof(hashTableEntry)), key, table.keySize);                       /*Copy the key into the area after the entry*/
-        next->value = memcpy(addPtr(next, sizeof(hashTableEntry) + table.keySize), value, table.valueSize); /*Copy the value into the area after the key*/
-        next->next = NULL;                                                                                  /*Make sure the next is set to NULL, to indicate that this is the last entry at the position*/
-        entry->next = next;                                                                                 /*Set previosly last entry to point at the new entry*/
+            return 0;                                                                                         /*Failed to allocate memory for the new entry*/
+        next->key = memcpy(addPtr(next, sizeof(hashTableEntry)), key, table->keySize);                        /*Copy the key into the area after the entry*/
+        next->value = memcpy(addPtr(next, sizeof(hashTableEntry) + table->keySize), value, table->valueSize); /*Copy the value into the area after the key*/
+        next->next = NULL;                                                                                    /*Make sure the next is set to NULL, to indicate that this is the last entry at the position*/
+        entry->next = next;                                                                                   /*Set previosly last entry to point at the new entry*/
+        table->count += 1;
         return 1;
     }
 }
 
-void htDelete(hashTable table, void *key)
+void htDelete(hashTable *table, void *key)
 {
-    hashTableEntry *entry = addPtr(table.data, _htGetOffset(table, _htGetIndex(table, key)));
+    hashTableEntry *entry = addPtr(table->data, _htGetOffset(table, _htGetIndex(table, key)));
     if (entry->key == NULL)
         return; /*Empty slot*/
     else
     {
-        if (memcmp(entry->key, key, table.keySize) == 0) /*Found it, and it's the head*/
+        if (memcmp(entry->key, key, table->keySize) == 0) /*Found it, and it's the head*/
         {
             if (entry->next == NULL)
             {                      /*There are no others*/
@@ -205,9 +168,10 @@ void htDelete(hashTable table, void *key)
             else
             {
                 hashTableEntry *next = entry->next;
-                memcpy(entry->key, next->key, table.keySize + table.valueSize); /*override the entry with the next one*/
+                memcpy(entry->key, next->key, table->keySize + table->valueSize); /*override the entry with the next one*/
                 entry->next = next->next;
                 free(next);
+                table->count -= 1;
                 return;
             }
         }
@@ -215,11 +179,12 @@ void htDelete(hashTable table, void *key)
         {
             while (entry->next != NULL)
             {
-                if (memcmp(entry->next->key, key, table.keySize) == 0) /*Found it*/
+                if (memcmp(entry->next->key, key, table->keySize) == 0) /*Found it*/
                 {
                     hashTableEntry *next = entry->next;
                     entry->next = next->next;
                     free(next);
+                    table->count -= 1;
                     return;
                 }
                 else
@@ -232,23 +197,70 @@ void htDelete(hashTable table, void *key)
     }
 }
 
-void htForEach(hashTable table, void (*f)(hashTableEntry))
-{
-    for (size_t i = 0; i < table.tableSize; i++)
-    {
-        hashTableEntry *entry = addPtr(table.data, _htGetOffset(table, i));
-        if (entry->key != NULL)
-        {
-            f(*entry);
+/*
+Works kinda like strtok
+Provide it with a hashtable to get the first entry in that table
+Provide it with a NULL ptr to get the next entry in the last provided hashtable
+Returns NULL when there are no more entries in the hashtable
 
-            while (entry->next != NULL)
-            {
-                entry = entry->next;
-                f(*entry);
-            }
+please only read the data, don't start changing it with this function, use the appropriate fuctions for that (htInsert, htDelete)
+*/
+hashTableEntry *htEnumerate(const hashTable *table)
+{
+    static int i = 0;
+    static int lastChainDepth = -1;
+    static hashTable *lastTablePtr = NULL;
+
+    if (table != NULL)
+    {
+        i = 0;
+        lastChainDepth = -1;
+        lastTablePtr = table;
+    }
+
+    if (lastTablePtr == NULL)
+        return NULL;
+
+    hashTable lastTable = *lastTablePtr;
+    if (i < lastTable.tableSize)
+    {
+        hashTableEntry *entry = addPtr(lastTable.data, _htGetOffset(&lastTable, i));
+
+        while (entry->key == NULL) /*Move to the next non empty slot in the table*/
+        {
+            ++i;
+            if (i < lastTable.tableSize)
+                entry = addPtr(lastTable.data, _htGetOffset(&lastTable, i));
+            else
+                return NULL;
+        }
+
+        int currentChainDepth = -1;
+
+        while (entry->next != NULL && currentChainDepth < lastChainDepth) /*Move to the last returned chain link*/
+        {
+            entry = entry->next;
+            ++currentChainDepth;
+        }
+        if (entry->next != NULL) /*Return next link in chain*/
+        {
+            lastChainDepth += 1;
+            return entry;
+        }
+        else /*We've reached the end of the chain*/
+        {
+            ++i;
+            lastChainDepth = -1;
+            return entry;
         }
     }
+    else
+    {
+        return NULL;
+    }
 }
+
+/*END_SECTION ht.h*/
 
 /* PROGRAM START */
 #define _BUFFER_SIZE 1024
@@ -322,24 +334,39 @@ void printTeamLine(team t)
     printf("%9.9s | %-9d || %9d | %-9d\n", t.teamName, t.points, t.goals, t.goalsAgainst);
 }
 
-void my_htPrint(hashTableEntry entry)
-{
-    printTeamLine(*(team *)entry.value);
-}
-
 team my_htGet(hashTable table, char teamName[4])
 {
     team res = {.points = 0, .goals = 0, .goalsAgainst = 0};
     char empty[4] = "\0\0\0";
     memcpy(res.teamName, empty, 4);
     memcpy(res.teamName, teamName, strlen(teamName));
-    void *temp = htGet(table, teamName);
+    void *temp = htGet(&table, teamName);
     if (temp != NULL)
     {
         res = *((team *)temp);
     }
 
     return res;
+}
+
+int compareTeams(const void *a, const void *b)
+{
+    team teamA = *(team *)a;
+    team teamB = *(team *)b;
+
+    if (teamA.points > teamB.points)
+        return -1;
+    else if (teamA.points < teamB.points)
+        return 1;
+    else
+    {
+        if ((teamA.goals - teamA.goalsAgainst) > (teamB.goals - teamB.goalsAgainst))
+            return -1;
+        else if ((teamA.goals - teamA.goalsAgainst) < (teamB.goals - teamB.goalsAgainst))
+            return 1;
+        else
+            return 0;
+    }
 }
 
 int main(void)
@@ -363,8 +390,8 @@ int main(void)
     }
     fclose(inputFile);
 
-    hashTable table = htInitTable(1, sizeof(char[4]), sizeof(team));
-
+    /*Calculate scores*/
+    hashTable table = htInitTable(25, sizeof(char[4]), sizeof(team));
     for (size_t i = 0; i < count; i++)
     {
         team first = my_htGet(table, games[i].firstTeamName);
@@ -386,11 +413,31 @@ int main(void)
             second.points += 1;
         }
 
-        htInsert(table, first.teamName, &first);
-        htInsert(table, second.teamName, &second);
+        htInsert(&table, first.teamName, &first);
+        htInsert(&table, second.teamName, &second);
     }
+
+    /*Sort scores*/
+    /*Hastable => array*/
+    team *teamArr = malloc(sizeof(team) * table.count);
+    assert(teamArr != NULL);
+    {
+        hashTableEntry *e = htEnumerate(&table);
+        int i = 0;
+        while (e != NULL)
+        {
+            memcpy(&teamArr[i++], e->value, sizeof(team));
+            e = htEnumerate(NULL);
+        }
+    }
+
+    qsort(teamArr, table.count, sizeof(team), compareTeams);
+
+    /*Print result table*/
     printf("%9.9s | %-9.9s || %9.9s | %-9.9s\n", "Name", "Points", "Goals", "GoalsVs");
     printf("%9.9s=|=%9.9s====%9.9s=|=%9.9s\n", "==============", "==============", "==============", "==============");
-
-    htForEach(table, my_htPrint);
+    for (size_t i = 0; i < table.count; i++)
+    {
+        printTeamLine(teamArr[i]);
+    }
 }
